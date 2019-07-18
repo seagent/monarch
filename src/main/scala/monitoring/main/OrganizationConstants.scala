c0aa8432819fd07b3141d833d9c8169968220998
burak <burakyonyul@gmail.com>
2019-07-18 21:35:18 +0300

    query rename

--- src/main/scala/monitoring/main/OrganizationConstants.scala
+++ src/main/scala/monitoring/main/OrganizationConstants.scala
@@ -4,16 +4,16 @@ object OrganizationConstants {
   /**
     * TODO: Sorgu federated yapılacak
     */
-  val STOCK_QUERY_TEMPLATE: String = "SELECT ?sameCompany ?count ?stock WHERE {" + "<%s> <http://www.w3.org/2002/07/owl#sameAs> ?sameCompany. " + "?sameCompany <http://data.nytimes.com/elements/associated_article_count> ?count." + "?sameCompany <http://stockmarket.com/elements/stockValue> ?value}"
+  val STOCK_QUERY_TEMPLATE: String = "SELECT ?nytCompany ?articleCount ?stockValue WHERE {" + "<%s> <http://www.w3.org/2002/07/owl#sameAs> ?nytCompany. " + "?nytCompany <http://data.nytimes.com/elements/associated_article_count> ?articleCount." + "?nytCompany <http://stockmarket.com/elements/stockValue> ?stockValue}"
 
   val RESOURCE_PATH = "src/main/resources/"
   val VOID_PATH: String = RESOURCE_PATH + "void/"
   val OWL_SAME_AS="http://www.w3.org/2002/07/owl#sameAs"
 
   def createStockQuery(companyURI: String, index: Int) = {
-    s"""SELECT ?sameCompany$index ?count$index ?stock$index WHERE {
-       |<$companyURI> <http://www.w3.org/2002/07/owl#sameAs> ?sameCompany$index.
-       |?sameCompany$index <http://data.nytimes.com/elements/associated_article_count> ?count$index.
-       |?sameCompany$index <http://stockmarket.com/elements/stockValue> ?value$index}""".stripMargin
+    s"""SELECT ?nytCompany$index ?articleCount$index ?stockValue$index WHERE {
+       |<$companyURI> <http://www.w3.org/2002/07/owl#sameAs> ?nytCompany$index.
+       |?nytCompany$index <http://data.nytimes.com/elements/associated_article_count> ?articleCount$index.
+       |?nytCompany$index <http://stockmarket.com/elements/stockValue> ?stockValue$index}""".stripMargin
   }
 }
